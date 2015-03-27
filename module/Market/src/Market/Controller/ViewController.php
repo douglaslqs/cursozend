4e3d9b2d932e4863239ddb012c612105cf33dac8
Douglas Santos <dsantos@linktelcorp.com>
2015-03-27 14:23:12 -0300

    M7

--- module/Market/src/Market/Controller/ViewController.php
+++ module/Market/src/Market/Controller/ViewController.php
@@ -8,13 +8,13 @@ class ViewController extends AbstractActionController {
     
     public function indexAction() {
         
-        $category = $this->params()->fromQuery('category');
+        $category = $this->params()->fromRoute('category');
         return new ViewModel(array('category' => $category));
     }
     
-    public function itemAction(){
+    public function itemAction() {
         
-        $itemId = $this->params()->fromQuery('itemId');
+        $itemId = $this->params()->fromRoute('itemId');
         
         if (!$itemId){
             $this->flashMessenger()->addMessage('Item Not Found');
