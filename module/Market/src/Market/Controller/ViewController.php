3acd46abe3aa65b53227e8185a9c1ff7113a177e
Douglas Santos <dsantos@linktelcorp.com>
2015-03-30 10:30:43 -0300

    Correcao da view item. M6

--- module/Market/src/Market/Controller/ViewController.php
+++ module/Market/src/Market/Controller/ViewController.php
@@ -14,6 +14,7 @@ class ViewController extends AbstractActionController {
     
     public function itemAction(){
         
+        
         $itemId = $this->params()->fromQuery('itemId');
         
         if (!$itemId){
