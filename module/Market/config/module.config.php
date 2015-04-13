1ff5aeda5d3a78c4aa06de5b07596020879e7fad
Douglas Santos <dsantos@linktelcorp.com>
2015-04-13 11:55:51 -0300

    resolução parcial segunda parte M9

--- module/Market/config/module.config.php
+++ module/Market/config/module.config.php
@@ -90,9 +90,40 @@ return array(
         'factories' => array(
             'market-post-form' => 'Market\Factory\PostFormFactory',
             'market-post-filter' => 'Market\Factory\PostFilterFactory'
-        )
-    ),
-
+        ),
+        'services' => array(
+            'date_expires' => array(
+                '2015-04-22',
+                '2015-05-22',
+                '2015-06-22',
+            ),
+            'market-expire-days' => array(
+                '1' => '1 day',
+                '5' => '5 days',
+                '10' => '10 days',
+                '20' => '20 days',
+            ),
+             'market-captcha-options' => array(
+                'titmeout' => 300 ,
+                'height' => '40',
+                'imgDir'    => __DIR__ . '/../../../public/img/captcha',
+                'imgUrl'    => '/img/captcha',
+                'font' => __DIR__ . '/../../../public/fonts/AliquamREG.ttf' ,
+            ),
+            /*
+            'market-captcha-options' => array(
+                'font' => __DIR__ . '/../../../data/fonts/arial.ttf',
+                'imgDir' => __DIR__ . '/../../../public/captcha',
+                'imgUrl' => '/captcha',
+                'fontSize' => 50,
+                'height' => 100,
+                'width' => 200,
+                'dotNoiseLevel' => 40,
+                'lineNoiseLevel' => 6
+            ),
+            */                     
+        ),
+    ),    
     'view_manager' => array(
         'template_path_stack' => array(
             __DIR__ . '/../view',
